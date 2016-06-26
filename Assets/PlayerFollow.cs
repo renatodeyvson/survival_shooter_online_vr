cb0a23f2868785bacc4b07d026a94adb6912b4dd
renatodeyvson <rdms@cin.ufpe.br>
2016-06-26 12:34:15 -0300

    stereo camera and camera position

---
+++ Assets/PlayerFollow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace CompleteProject
+{
+    public class PlayerFollow : MonoBehaviour
+    {
+        public Transform target;            // The position that that camera will be following.
+
+        void Start()
+        {
+            
+        }
+
+
+        void FixedUpdate()
+        {
+            //Iguala a rotação
+            transform.rotation = target.rotation;
+        }
+    }
+}
---
+++ Assets/_CompletedAssets/Scripts/Player/PlayerFollow1.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace CompleteProject
+{
+    public class PlayerFollow1 : MonoBehaviour
+    {
+        public Transform target;            // The position that that camera will be following.
+
+        void Start()
+        {
+
+        }
+
+
+        void FixedUpdate()
+        {
+            //Iguala a rotação
+            transform.rotation = target.rotation;
+        }
+    }
+}
