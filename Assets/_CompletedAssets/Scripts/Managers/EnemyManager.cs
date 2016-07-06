61a1c72eb6db9bb4e04aaf116f2cee345567f19b
Arthur Pires <Arthur Pires>
2016-07-05 22:07:18 -0300

    spawning in server and client same monsters

--- Assets/_CompletedAssets/Scripts/Managers/EnemyManager.cs
+++ Assets/_CompletedAssets/Scripts/Managers/EnemyManager.cs
@@ -1,21 +1,31 @@
 ﻿using UnityEngine;
+using UnityEngine.Networking;
 
 namespace CompleteProject
 {
-    public class EnemyManager : MonoBehaviour
+    public class EnemyManager : NetworkBehaviour
     {
         public PlayerHealth playerHealth;       // Reference to the player's heatlh.
         public GameObject enemy;                // The enemy prefab to be spawned.
         public float spawnTime = 3f;            // How long between each spawn.
         public Transform[] spawnPoints;         // An array of the spawn points this enemy can spawn from.
+        public const int MONSTER_GROUP = 3;
+        private bool isCalling = false;
 
-
-        void Start ()
+        void Update()
         {
             // Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
-            InvokeRepeating ("Spawn", spawnTime, spawnTime);
-        }
+            if (!isCalling)
+            {
+                if (isServer)
+                {
+                    InvokeRepeating("Spawn", spawnTime, spawnTime);
+                    isCalling = true;
+                }
+                Debug.Log("spawning?");
+            }
 
+        }
 
         void Spawn ()
         {
@@ -29,9 +39,9 @@ namespace CompleteProject
 
             // Find a random index between zero and one less than the number of spawn points.
             int spawnPointIndex = Random.Range (0, spawnPoints.Length);
-
+            GameObject go = (GameObject)GameObject.Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
             // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
-            Instantiate (enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+            NetworkServer.Spawn(go);
         }
     }
 }
